b95eea374663668a32544448fad729fe8f3fb47d
Kat2 <main@kat2.net>
2016-11-07 13:23:49 +0000

    get handler

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-echo getDomain();
+echo getHandler(getDomain());
+
+function getHandler($domain){
+    $types = array(
+        '1' => 'RedirectSimple',
+        '2' => 'RedirectElaborate',
+        '3' => 'ParkSimple',
+        '4' => 'ParkElaborate'
+    );
+
+    $type = '1';
+    if($domain == 'kat2-parker.herokuapp.com'){$type = '1';}
+    if($domain == 'empetree.com'){$type = '1';}
+    if($domain == 'qloud.live'){$type = '1';}
+    if($domain == 'qloud.site'){$type = '1';}
+    if($domain == 'ratdir.com'){$type = '1';}
+
+    return $types[$type];
+}
 
 function getDomain(){
     global $_SERVER;
