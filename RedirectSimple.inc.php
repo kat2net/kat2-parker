bc4d7eac5d70c8ff66431caf29720d6dde24023e
Kat2 <main@kat2.net>
2016-11-07 14:04:07 +0000

    forced domain!

--- RedirectSimple.inc.php
+++ RedirectSimple.inc.php
@@ -4,7 +4,7 @@ echo gotoAdFly();
 function gotoAdFly(){
     global $domain;
 
-    $url = file_get_contents('https://api.adf.ly/v1/shorten?&_user_id=7321466&_api_key=f9e524c57f6696b19bf7bb121d094fb8&url='.urlencode('http://'.$domain.'/'));
+    $url = file_get_contents('https://api.adf.ly/v1/shorten?&_user_id=7321466&_api_key=f9e524c57f6696b19bf7bb121d094fb8*domain=adf.ly&url='.urlencode('http://'.$domain.'/'));
 
     return $url;
 }
