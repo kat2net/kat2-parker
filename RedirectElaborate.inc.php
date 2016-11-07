dc298e4252303b964ab391ec5303011456d53fc5
Kat2 <main@kat2.net>
2016-11-07 18:53:31 +0000

    x

--- RedirectElaborate.inc.php
+++ RedirectElaborate.inc.php
@@ -6,22 +6,20 @@ if((isFromAdFly()) || (isFromAdultxyz()) || (isFromCoinURL()) || (isFromShortest
     if((isFromAdultxyz()) || (isFromCoinURL()) || (isFromShortest())){
         if((isFromCoinURL()) || (isFromShortest())){
             if(isFromShortest()){
-                echo 'done';
+                require_once('ParkSimple.inc.php');
             }else{
-                echo gotoShortest()."\n";
-                //header('Location: '.gotoShortest());
+                //echo gotoShortest()."\n";
+                header('Location: '.gotoShortest());
             }
         }else{
-            echo gotoCoinURL()."\n";
-            //header('Location: '.gotoCoinURL());
+            //echo gotoCoinURL()."\n";
+            header('Location: '.gotoCoinURL());
         }
     }else{
-        echo gotoAdultxyz()."\n";
-        //header('Location: '.gotoAdultxyz());
+        //echo gotoAdultxyz()."\n";
+        header('Location: '.gotoAdultxyz());
     }
 }else{
-    echo gotoAdFly()."\n";
-    //header('Location: '.gotoAdFly());
+    //echo gotoAdFly()."\n";
+    header('Location: '.gotoAdFly());
 }
-
-print_r($_SERVER);
