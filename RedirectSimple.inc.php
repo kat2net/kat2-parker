e5cab9d69134aaa2343730064ded44a0dde75c95
Kat2 <main@kat2.net>
2016-11-07 14:05:29 +0000

    fix error

--- RedirectSimple.inc.php
+++ RedirectSimple.inc.php
@@ -5,7 +5,7 @@ print_r($_SERVER);
 function gotoAdFly(){
     global $domain;
 
-    $url = file_get_contents('https://api.adf.ly/v1/shorten?&_user_id=7321466&_api_key=f9e524c57f6696b19bf7bb121d094fb8*domain=adf.ly&url='.urlencode('http://'.$domain.'/'));
+    $url = file_get_contents('https://api.adf.ly/v1/shorten?&_user_id=7321466&_api_key=f9e524c57f6696b19bf7bb121d094fb8&domain=adf.ly&url='.urlencode('http://'.$domain.'/'));
 
     return $url;
 }
